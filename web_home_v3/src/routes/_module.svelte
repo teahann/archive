278596e4f120c55a8d2d5cdac42196e6b2937d3f
unknown <26565276+teahann@users.noreply.github.com>
2024-03-10 08:54:05 -0600

    archive home v3

---
+++ web_home_v3/src/routes/_module.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { onMount } from 'svelte'
+  import { isActive } from '@roxi/routify'
+  import { load_bg } from '@/utils/bg.js'
+  
+  // Load bg url from localStorage
+  onMount(() => {
+    document.readyState === 'complete' ? load_bg() : window.addEventListener('load', load_bg);
+  })
+
+  const links = [
+    ['🏡', 'index', 'Home'],
+    ['🎧', 'playlist', 'Curated YouTube Videos'],
+    ['📝', 'post', 'Random words assembled together'],
+    ['🔗', 'link', 'Other sites to check out'],
+    ['🛒', 'shop', 'Buy stuff'],
+    ['🎼', 'daw', 'Play a synth of drum kit'],
+    ['💾', 'settings', 'Customize this site']
+  ]
+</script>
+
+<main>
+  <slot />
+</main>
+
+<nav>
+  {#each links as [label, url, desc]}
+    <a href={`/${url}`} class:Open={$isActive(url)} title={desc}>{label}</a>
+  {/each}
+</nav>
---
+++ web_home_v3/src/routes/link/index.svelte
@@ -0,0 +1,5 @@
+<div id="Links" class="Browser">
+  <a class="Home" href="./link/external">External</a>
+  <a class="Home" href="./link/opensource">Opensource</a>
+  <a class="Home" href="./link/web">Web</a>
+</div>
